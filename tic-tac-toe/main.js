ad33c56731214f7ccd36e6d8297172b43458264a
Soumil Roy <hello@soumilroy.com>
2023-03-18 23:29:49 +0530

    unoptimized winner

--- tic-tac-toe/main.js
+++ tic-tac-toe/main.js
@@ -1,11 +1,10 @@
 import "./style.css";
 
-const ROWS_COUNT = 3;
-const COLS_COUNT = ROWS_COUNT;
+const ROWS_COLS_COUNT = 3;
 
-const gameArr = Array(ROWS_COUNT)
+const gameArr = Array(ROWS_COLS_COUNT)
   .fill(undefined)
-  .map(() => Array(COLS_COUNT).fill(undefined));
+  .map(() => Array(ROWS_COLS_COUNT).fill(undefined));
 
 const populateGameArray = (row, col, val) => {
   gameArr[row][col] = val;
@@ -13,12 +12,76 @@ const populateGameArray = (row, col, val) => {
   console.log(gameArr);
 };
 
-const checkForWinner = () => {
-  if (gameArr[0][0] == gameArr[1][1] && gameArr[0][0] == gameArr[2][2]) {
-    return gameArr[0][0];
+const checkForWinner = (symbol) => {
+  let winner;
+
+  // Diagonals
+  if (
+    symbol == gameArr[0][0] &&
+    gameArr[0][0] == gameArr[1][1] &&
+    gameArr[0][0] == gameArr[2][2]
+  ) {
+    winner = gameArr[0][0];
   }
 
-  return null;
+  if (
+    symbol == gameArr[0][2] &&
+    gameArr[0][2] == gameArr[1][1] &&
+    gameArr[0][2] == gameArr[2][0]
+  ) {
+    winner = gameArr[0][2];
+  }
+
+  // Rows
+  if (
+    symbol == gameArr[0][0] &&
+    gameArr[0][0] == gameArr[0][1] &&
+    gameArr[0][0] == gameArr[0][2]
+  ) {
+    winner = gameArr[0][0];
+  }
+
+  if (
+    symbol == gameArr[1][0] &&
+    gameArr[1][0] == gameArr[1][1] &&
+    gameArr[1][0] == gameArr[1][2]
+  ) {
+    winner = gameArr[1][0];
+  }
+
+  if (
+    symbol == gameArr[2][0] &&
+    gameArr[2][0] == gameArr[2][1] &&
+    gameArr[2][0] == gameArr[2][2]
+  ) {
+    winner = gameArr[2][0];
+  }
+
+  if (
+    symbol == gameArr[0][0] &&
+    gameArr[0][0] == gameArr[1][0] &&
+    gameArr[0][0] == gameArr[2][0]
+  ) {
+    winner = gameArr[0][0];
+  }
+
+  if (
+    symbol == gameArr[1][1] &&
+    gameArr[0][1] == gameArr[1][1] &&
+    gameArr[0][1] == gameArr[2][1]
+  ) {
+    winner = gameArr[0][1];
+  }
+
+  if (
+    symbol == gameArr[0][2] &&
+    gameArr[0][2] == gameArr[1][2] &&
+    gameArr[0][2] == gameArr[2][2]
+  ) {
+    winner = gameArr[0][2];
+  }
+
+  return winner ? winner : null;
 };
 
 const captureClick = (e) => {
@@ -39,8 +102,8 @@ const captureClick = (e) => {
 const renderBoard = () => {
   let btnMarkup = ``;
 
-  for (let i = 0; i < ROWS_COUNT; i++) {
-    for (let j = 0; j < ROWS_COUNT; j++) {
+  for (let i = 0; i < ROWS_COLS_COUNT; i++) {
+    for (let j = 0; j < ROWS_COLS_COUNT; j++) {
       btnMarkup += `<button data-row="${i}" data-column="${j}"></button>`;
     }
   }
