c0075fed714f8ace25bd0e2d028726cc3e110f6e
Soumil Roy <hello@soumilroy.com>
2023-06-15 10:56:19 +0530

    canvas lines

--- canvas-practice/src/main.ts
+++ canvas-practice/src/main.ts
@@ -1,24 +1,51 @@
-import './style.css'
-import typescriptLogo from './typescript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.ts'
+import "./style.css";
 
-document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+document.addEventListener("DOMContentLoaded", function () {
+  const canvas = document.querySelector("#canvas") as HTMLCanvasElement;
+  const ctx = canvas.getContext("2d");
 
-setupCounter(document.querySelector<HTMLButtonElement>('#counter')!)
+  if (!ctx) throw new Error("Context not defined");
+
+  // Draw line steps
+  // ctx.beginPath(); // reset context state
+  // ctx.moveTo(30, 60); // x, y
+  // ctx.lineTo(90, 100); // x, y
+  // ctx.strokeStyle = "yellow";
+  // ctx.lineWidth = 10;
+  // ctx.stroke();
+
+  // ctx.beginPath(); // reset context state
+  // ctx.moveTo(30, 50); // x, y
+  // ctx.lineTo(80, 100); // x, y
+  // ctx.lineTo(130, 30); // x, y
+  // ctx.lineTo(180, 30); // x, y
+  // ctx.lineTo(230, 90); // x, y
+  // ctx.strokeStyle = "yellow";
+  // ctx.lineWidth = 4;
+  // // ctx.lineCap = "round";
+  // ctx.stroke();
+
+  // Line joins
+  // ctx.beginPath();
+  // ctx.lineWidth = 10;
+  // ctx.moveTo(30, 30);
+  // ctx.lineTo(130, 30);
+  // ctx.lineTo(130, 90);
+  // ctx.lineTo(230, 90);
+  // ctx.lineTo(230, 30);
+  // ctx.lineJoin = "round";
+  // ctx.stroke();
+
+  // add shadow
+
+  ctx.beginPath();
+  ctx.lineWidth = 10;
+  ctx.strokeStyle = "red";
+  ctx.moveTo(30, 30);
+  ctx.lineTo(130, 30);
+  ctx.shadowColor = "red";
+  ctx.shadowOffsetX = 0;
+  ctx.shadowOffsetY = 0;
+  ctx.shadowBlur = 10;
+  ctx.stroke();
+});
